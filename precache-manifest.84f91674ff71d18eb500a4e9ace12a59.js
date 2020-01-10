1ea9ed6bf4bbc2c35f03d9306830e21b2c741b76
Vlad Axinte <vlad.axinte93@gmail.com>
2020-01-10 13:52:35 +0100

    Updates

---
+++ precache-manifest.84f91674ff71d18eb500a4e9ace12a59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6d1703698f889b0b77c0f829cb59d7",
+    "url": "/spring-onion/index.html"
+  },
+  {
+    "revision": "34be89cae6bbd412285c",
+    "url": "/spring-onion/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dcba7585773c54e98c7d",
+    "url": "/spring-onion/static/js/2.f3622a69.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/spring-onion/static/js/2.f3622a69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34be89cae6bbd412285c",
+    "url": "/spring-onion/static/js/main.304044f5.chunk.js"
+  },
+  {
+    "revision": "9afe3983ba01af8a4a66",
+    "url": "/spring-onion/static/js/runtime-main.a9868610.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spring-onion/static/media/logo.5d5d9eef.svg"
+  }
+]);
