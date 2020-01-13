2dae474ad85fa787bdbd555faf72be8c307c6adf
Vlad Axinte <vlad.axinte93@gmail.com>
2020-01-13 16:07:30 +0100

    -

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      a
+      asdf
     </div>
   );
 }
